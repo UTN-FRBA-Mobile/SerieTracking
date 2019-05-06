aa44eab8f2c890ddf88131aeedcb37d30f28fc6a
nicogon <nicogon@Nicolass-MacBook.local>
2019-05-06 13:27:18 -0300

    swipe

--- app/src/main/java/com/example/serietracking/MainActivity.kt
+++ app/src/main/java/com/example/serietracking/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.example.serietracking
+import android.graphics.Canvas
 import kotlinx.android.synthetic.main.activity_main.*
 
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -19,6 +20,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ItemTouchHelper
+
+import androidx.recyclerview.widget.RecyclerView
+
+
 import com.bumptech.glide.Glide
 
 //data class Capitulo(val title: String, val year: Int, val image: String)
@@ -74,12 +80,86 @@ class MainActivity : AppCompatActivity(), Callback<TVModel> {
 
 
         Glide.with(this).load("https://image.tmdb.org/t/p/w500/iflq7ZJfso6WC7gk9l1tD3unWK.jpg").into(imageView)
-
+        initSwipe()
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
         ApiClient.apiInterface.getPopular(HttpConstants.API_KEY).enqueue(this)
 
 
     }
+
+
+    private fun initSwipe() {
+
+     //   recyclerView = findViewById(R.id.card_recycler_view) as RecyclerView
+
+        val simpleItemTouchCallback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+
+            override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
+                return false
+            }
+
+
+
+            override fun onChildDraw(c: Canvas, recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, dX: Float, dY: Float, actionState: Int, isCurrentlyActive: Boolean) {
+
+            //    val icon: Bitmap
+                if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+
+                    val itemView = viewHolder.itemView
+                    val height = itemView.bottom.toFloat() - itemView.top.toFloat()
+                    val width = height / 3
+
+                    if (dX > 0) {
+
+                        Log.d("log","AAA")
+
+                    //    p.color = Color.parseColor("#388E3C")
+                     //   val background = RectF(itemView.left.toFloat(), itemView.top.toFloat(), dX, itemView.bottom.toFloat())
+                     //   c.drawRect(background, p)
+                     ////   icon = BitmapFactory.decodeResource(resources, R.drawable.ic_edit_white)
+                     //   val icon_dest = RectF(itemView.left.toFloat() + width, itemView.top.toFloat() + width, itemView.left.toFloat() + 2 * width, itemView.bottom.toFloat() - width)
+                     //   c.drawBitmap(icon, null, icon_dest, p)
+                    } else {
+
+
+                        Log.d("log","BBB")
+                      //  p.color = Color.parseColor("#D32F2F")
+                     //   val background = RectF(itemView.right.toFloat() + dX, itemView.top.toFloat(), itemView.right.toFloat(), itemView.bottom.toFloat())
+                      //  c.drawRect(background, p)
+                      //  icon = BitmapFactory.decodeResource(resources, R.drawable.ic_delete_white)
+                      //  val icon_dest = RectF(itemView.right.toFloat() - 2 * width, itemView.top.toFloat() + width, itemView.right.toFloat() - width, itemView.bottom.toFloat() - width)
+                       // c.drawBitmap(icon, null, icon_dest, p)
+                    }
+                }
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+            }
+
+
+
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                Log.d("log","FGF")
+                if (direction == ItemTouchHelper.LEFT) {
+                  //  adapter!!.removeItem(position)
+                } else {
+                 //   removeView()
+                   // edit_position = position
+                  //  alertDialog!!.setTitle("Edit Name")
+                  //  et_name!!.setText(names[position])
+                  //  alertDialog!!.show()
+                }
+            }
+
+
+
+        }
+
+        val itemTouchHelper = ItemTouchHelper(simpleItemTouchCallback)
+        itemTouchHelper.attachToRecyclerView(list_recycler_view)
+
+
+        }
 
     override fun onResponse(call: Call<TVModel>?, response: Response<TVModel>?) {
         if (response!!.isSuccessful) {
