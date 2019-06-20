c3807bd3b69622845c31915345e69da87e0d0b8d
nicogon <nicogon@Nicolass-MacBook.local>
2019-06-20 17:03:38 -0300

    fix crash

--- app/src/main/java/com/example/serietracking/RichEpisode.kt
+++ app/src/main/java/com/example/serietracking/RichEpisode.kt
@@ -1,7 +1,32 @@
 package com.example.serietracking
 
-class RichEpisode {
+import android.os.Parcel
+import android.os.Parcelable
+
+class RichEpisode() : Parcelable {
     var tv: TVCompleteModel? = null
     var season: SeasonModel? = null
     var episode: Episode? = null
+
+    constructor(parcel: Parcel) : this() {
+
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<RichEpisode> {
+        override fun createFromParcel(parcel: Parcel): RichEpisode {
+            return RichEpisode(parcel)
+        }
+
+        override fun newArray(size: Int): Array<RichEpisode?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
