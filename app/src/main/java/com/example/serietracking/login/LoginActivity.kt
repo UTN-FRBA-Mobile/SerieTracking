0cc53e23b20bbcf28af0361103f4d41d966b0240
Gabriel Gartensztern <ggartensztern@despegar.com>
2019-06-29 11:55:18 -0300

    Login de verdad y saco web view

--- app/src/main/java/com/example/serietracking/login/LoginActivity.kt
+++ app/src/main/java/com/example/serietracking/login/LoginActivity.kt
@@ -3,11 +3,7 @@ package com.example.serietracking.login
 import android.os.Bundle
 import android.view.View
 import android.content.Intent
-import android.graphics.Bitmap
 import android.util.Log
-import android.webkit.WebResourceRequest
-import android.webkit.WebView
-import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import com.example.serietracking.MainActivity
 import com.example.serietracking.R
@@ -19,6 +15,7 @@ import com.example.serietracking.login.dto.RequestTokenResponse
 import com.example.serietracking.network.ApiClient
 import com.example.serietracking.network.ErrorLoggingCallback
 import com.example.serietracking.network.HttpConstants
+import kotlinx.android.synthetic.main.activity_login.*
 import retrofit2.Call
 import retrofit2.Response
 
@@ -31,14 +28,12 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun executeLogin(view: View) {
-        //val intent = Intent(this@LoginActivity, LoginWebViewActivity::class.java)
-        //startActivity(intent)
         Log.d("log","calling create request token")
         ApiClient.apiInterface.createRequestToken(HttpConstants.API_KEY).enqueue(object: ErrorLoggingCallback<RequestTokenResponse>() {
             override fun onResponse(call: Call<RequestTokenResponse>, response: Response<RequestTokenResponse>) {
                 Log.d("log","create request token finished")
                 response.body().let { requestTokenResponse ->
-                    ApiClient.apiInterface.validateRequestTokenLogin(HttpConstants.API_KEY, CreateSessionLoginRequest("gabigart", "mobile", requestTokenResponse.requestToken)).enqueue(object: ErrorLoggingCallback<RequestTokenResponse>() {
+                    ApiClient.apiInterface.validateRequestTokenLogin(HttpConstants.API_KEY, CreateSessionLoginRequest(user.text.toString(), pass.text.toString(), requestTokenResponse.requestToken)).enqueue(object: ErrorLoggingCallback<RequestTokenResponse>() {
                         override fun onResponse(call: Call<RequestTokenResponse>, response: Response<RequestTokenResponse>) {
                             ApiClient.apiInterface.createSession(
                                 HttpConstants.API_KEY,
